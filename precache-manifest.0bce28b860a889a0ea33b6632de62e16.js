9b7ee22c65f1cfd1fc5742263bbf74b97c551393
Arsh <arshdeepgary@gmail.com>
2020-06-26 17:11:07 -0700

    Updates

---
+++ precache-manifest.0bce28b860a889a0ea33b6632de62e16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee82ebb2279a29413a1fdc661d8c6c9e",
+    "url": "/simple-poll/index.html"
+  },
+  {
+    "revision": "3894dcec97c9111d5b0d",
+    "url": "/simple-poll/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d3fe3aae26d85f044585",
+    "url": "/simple-poll/static/js/2.dc9c4dca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simple-poll/static/js/2.dc9c4dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3894dcec97c9111d5b0d",
+    "url": "/simple-poll/static/js/main.ab1b5e08.chunk.js"
+  },
+  {
+    "revision": "de1d07754bb4347d7fe0",
+    "url": "/simple-poll/static/js/runtime-main.75304081.js"
+  }
+]);
